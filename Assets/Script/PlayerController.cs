cd43175bdf144e7f48367c30c48aedbabf97b8f8
joywu001 <joywu001@gmail.com>
2024-09-30 10:52:07 +0100

    1.2 script

---
+++ Assets/Script/PlayerController.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditorInternal;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerController : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    public Vector2 moveValue;
+    public float speed;
+    void OnMove(InputValue value)
+    {
+        moveValue = value.Get<Vector2>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        Vector3 movement = new Vector3(moveValue.x, 0.0f, moveValue.y);
+
+        GetComponent<Rigidbody>().AddForce(movement*speed*Time.fixedDeltaTime);
+    }
+}
